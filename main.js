2b52ad5132e038aa81a15bfd6cd4171917649462
scottsilver <scottsilver47@gmail.com>
2019-06-02 16:18:36 -0500

    obtained reference to all article_header elements and changed classlist property to important.

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 // Use JavaScript to obtain a reference to the first section with the class of article_header and change its text with the textContent property to "Welcome to the {insert your name here} blog"
 const sectionOne=document.querySelector(".article__header")
 console.log(sectionOne);
-const blogTextContent = sectionOne.textContent
-console.log(blogTextContent)
-const blogText = "This doesn't work"
-console.log(blogText)
 sectionOne.textContent="Welcome to Scott's blog"
+
+const allHeaders=document.querySelectorAll(".article__header")
+console.log(allHeaders)
+for (let i = 0; i < allHeaders.length; i++) {
+    allHeaders[i].classList.add("important")
+    console.log(allHeaders[i]);
+    console.log(allHeaders.cl)
+}
+console.log(sectionOne.classList)
+// const blogTextContent = sectionOne.textContent
+// console.log(blogTextContent)
+// const blogText = "This doesn't work"
+// console.log(blogText)
+// sectionOne.textContent="Welcome to Scott's blog"
